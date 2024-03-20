e79d2a683fe95c5fcd4c774a5bb352fb5fcc41f2
NataGorzynska <153229305+NataGorzynska@users.noreply.github.com>
2024-03-20 22:15:05 +0100

    added basic javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,10 @@
+let durbanElement = document.querySelector("#durban");
+let durbanDateElement = durbanElement.querySelector(".date");
+let durbanTimeElement = durbanElement.querySelector(".time");
+let durbanTime = moment();
 
+durbanDateElement.innerHTML = durbanTime.format("MMMM Do YYYY");
+
+durbanTimeElement.innerHTML = `${durbanTime.format(
+  "h:mm:ss"
+)} <small> ${durbanTime.format("A")} </small>`;
