55a5cd0d5f25f426551fd08237747f330efca405
NataGorzynska <153229305+NataGorzynska@users.noreply.github.com>
2024-03-21 23:45:09 +0100

    added timezones

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,25 @@
-let durbanElement = document.querySelector("#durban");
-let durbanDateElement = durbanElement.querySelector(".date");
-let durbanTimeElement = durbanElement.querySelector(".time");
-let durbanTime = moment();
+function updateTime() {
+  let durbanElement = document.querySelector("#durban");
+  let durbanDateElement = durbanElement.querySelector(".date");
+  let durbanTimeElement = durbanElement.querySelector(".time");
+  let durbanTime = moment().tz("Africa/Johannesburg");
 
-durbanDateElement.innerHTML = durbanTime.format("MMMM Do YYYY");
+  durbanDateElement.innerHTML = durbanTime.format("MMMM Do YYYY");
 
-durbanTimeElement.innerHTML = `${durbanTime.format(
-  "h:mm:ss"
-)} <small> ${durbanTime.format("A")} </small>`;
+  durbanTimeElement.innerHTML = `${durbanTime.format(
+    "h:mm:ss"
+  )} <small> ${durbanTime.format("A")} </small>`;
+
+  let exeterElement = document.querySelector("#exeter");
+  let exeterDateElement = exeterElement.querySelector(".date");
+  let exeterTimeElement = exeterElement.querySelector(".time");
+  let exeterTime = moment().tz("Europe/London");
+
+  exeterDateElement.innerHTML = exeterTime.format("MMMM Do YYYY");
+
+  exeterTimeElement.innerHTML = `${exeterTime.format(
+    "h:mm:ss"
+  )} <small> ${exeterTime.format("A")} </small>`;
+}
+updateTime();
+setInterval(updateTime, 1000);
